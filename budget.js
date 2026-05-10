393c016dea90cae2a6cf82fea70583d6cf3fd18b
ssadfafs <1959867628@qq.com>
2026-05-10 19:57:56 +0800

    test: add Jest tests with 84% coverage and Codecov integration

--- budget.js
+++ budget.js
@@ -303,4 +303,10 @@ function inactive(elements) {
   });
 }
 
-applyTranslations(currentLang);
+if (typeof applyTranslations === "function" && typeof currentLang !== "undefined") {
+  applyTranslations(currentLang);
+}
+
+if (typeof module !== "undefined") {
+  module.exports = { calculateTotal, calculateBalance };
+}
